faf674ffc6d7dd634829a2563d628ad656f1a71f
vishwas3332 <vishwas3332@gmail.com>
2023-09-23 20:31:53 +0530

    deleted bootstrap and added styles

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,104 +1,79 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000
+const readLine = require('readline');
 
 let dbURL = 'mongodb://127.0.0.1:27017/Loc8r';
-if (process.env.NODE_ENV === 'production') {
-  dbURL = process.env.DB_HOST || process.env.MONGODB_URI;
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB Connected: ' + process.env.MONGO_URI);
+    } catch (error) {
+        console.log('Error: ' + error);
+        process.exit(1);
+    }
 }
 
-mongoose.connect(dbURL, {useNewUrlParser: true});
+const connect = () => {
+    setTimeout(() => mongoose.connect(dbURL), 1000);
+}
 
 mongoose.connection.on('connected', () => {
- console.log(`Mongoose connected to ${dbURL}`);
+    console.log('Mongoose connected to ' + dbURL);
 });
 
 mongoose.connection.on('error', err => {
- console.log(`Mongoose connection error: ${err}`);
+    console.log('Error: ' + err);
+    return connect();
 });
 
 mongoose.connection.on('disconnected', () => {
- console.log('Mongoose disconnected');
+    console.log('Disconnected');
 });
 
-const gracefulShutdown = (msg, callback) => {
-  mongoose.connection.close( () => {
-    console.log(`Mongoose disconnected through ${msg}`);
-    callback();
-  });
+if (process.platform === 'win32') {
+    const rl = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on('SIGINT', () => {
+        process.emit("SIGINT");
+    });
+}
+
+async function gracefulShutdown(msg) {
+    const closed = await mongoose.connection.close()
+        .then(function () {
+            console.log(`Mongoose disconnected through ${msg}`);
+            process.exit();
+        });
 };
-// For nodemon restarts
+
 process.once('SIGUSR2', () => {
     gracefulShutdown('nodemon restart', () => {
         process.kill(process.pid, 'SIGUSR2');
     });
 });
-// For app termination
 process.on('SIGINT', () => {
     gracefulShutdown('app termination', () => {
         process.exit(0);
     });
 });
+process.on('SIGTERM', () => {
+    gracefulShutdown('Cyclic app shutdown', () => {
+        process.exit(0);
+    });
+});
 
-
-// const mongoose = require('mongoose');
-// const readLine = require('readline');
-
-// let dbURL = 'mongodb://127.0.0.1/Loc8r';
-// if (process.env.NODE_ENV === 'production') {
-//   dbURL = process.env.DB_HOST || process.env.MONGODB_URI;
-// }
-
-// const connect = () => {
-//   setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }), 1000);
-// }
-
-// mongoose.connection.on('connected', () => {
-//   console.log('connected');
-// });
-
-// mongoose.connection.on('error', err => {
-//   console.log('error: ' + err);
-//   return connect();
-// });
-
-// mongoose.connection.on('disconnected', () => {
-//   console.log('disconnected');
-// });
-
-// if (process.platform === 'win32') {
-//   const rl = readLine.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   });
-//   rl.on ('SIGINT', () => {
-//     process.emit("SIGINT");
-//   });
-// }
-
-// const gracefulShutdown = (msg, callback) => {
-//   mongoose.connection.close( () => {
-//     console.log(`Mongoose disconnected through ${msg}`);
-//     callback();
-//   });
-// };
-
-// process.once('SIGUSR2', () => {
-//   gracefulShutdown('nodemon restart', () => {
-//     process.kill(process.pid, 'SIGUSR2');
-//   });
-// });
-// process.on('SIGINT', () => {
-//   gracefulShutdown('app termination', () => {
-//     process.exit(0);
-//   });
-// });
-// process.on('SIGTERM', () => {
-//   gracefulShutdown('Heroku app shutdown', () => {
-//     process.exit(0);
-//   });
-// });
-
-// connect();
-
-// require('./locations');
+if (process.env.NODE_ENV === 'production') {
+    connectDB().then(() => {
+        app.listen(PORT, () => {
+            console.log("listening for requests");
+        })
+    })
+}
+else {
+    connect();
+}
+require('./locations');
