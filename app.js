5ba98df4b70c8f926654703e8eec7060342a4ebc
Vishwas Reddy <127083587+vishwas3332@users.noreply.github.com>
2023-09-24 16:01:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ app.use(function(err, req, res, next) {
 });
 
 // required for nodemon listen
-app.listen(3000 , ()=>{
-  console.log('Server is listening on port 3000');
-});
+// app.listen(3000 , ()=>{
+//   console.log('Server is listening on port 3000');
+// });
 
 module.exports = app;
