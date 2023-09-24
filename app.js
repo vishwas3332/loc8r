7daec69e662ea9a79364fa205c382d62cf1d6018
vishwas3332 <vishwas3332@gmail.com>
2023-09-24 15:57:16 +0530

    created app_api

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var routesApi = require('./app_api/routes/index');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname,'app_server','views'));
 app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', routesApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,6 +41,11 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// required for nodemon listen
+app.listen(3000 , ()=>{
+  console.log('Server is listening on port 3000');
 });
 
 module.exports = app;
